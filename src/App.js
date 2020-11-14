54fbd00079a5731583f31d1052617431046bffb6
Giorgio Frittoli <giorgio.frittoli@gmail.com>
2020-11-14 14:27:47 +0100

    hide div with tern exp

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false,
   };
 
   switchNameHandler = (newName) => {
@@ -34,6 +35,11 @@ class App extends Component {
     });
   }
 
+  togglePerson = () => {
+    const isShow = this.state.showPersons;
+    this.setState({ showPersons: !isShow });
+  }
+
   render() {
 
     const style = {
@@ -49,23 +55,27 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <button style={style}
-          onClick={() => this.switchNameHandler("Maxxiii")}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, "AUUUUU")}
-          change={this.nameChangedHandler}
-        >
-          My Hobbies: Racing
+          onClick={this.togglePerson}>Toggle persons</button>
+        {this.state.showPersons ?
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+            />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, "AUUUUU")}
+              change={this.nameChangedHandler}
+            >
+              My Hobbies: Racing
         </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+            />
+          </div> : null
+        }
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));

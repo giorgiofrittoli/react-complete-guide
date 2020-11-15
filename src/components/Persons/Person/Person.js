701d29904a54fbcf4622869b3c5eee2c665c6fce
Giorgio Frittoli <giorgio.frittoli@gmail.com>
2020-11-15 17:54:09 +0100

    hooks

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,7 +3,28 @@ import styles from "./Person.module.css";
 
 class Person extends Component {
 
+    static getDerivedStateFromProps(props, state) {
+        console.log("[Person.js] getDerivedStateFromProps");
+        return state;
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("[Person.js] shouldComponentUpdate");
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log("[Person.js] getSnapshotBeforeUpdate");
+        return { message: "snapshot" }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("[Person.js] componentDidUpdate");
+        console.log(snapshot);
+    }
+
     render() {
+        console.log("[Person.js] render");
         return (
             <div className={styles.Person} >
                 <p onClick={this.props.click} > I'm {this.props.name} and a i am {this.props.age} years old!</p>
